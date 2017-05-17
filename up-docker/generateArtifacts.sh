2b98955125dacc4b65c0f9daf394272c6b71f4ff
Honglei Cong <conghonglei@gmail.com>
2017-05-17 10:31:40 +0800

    init commit

---
+++ up-docker/generateArtifacts.sh
@@ -0,0 +1,56 @@
+#!/bin/bash +x
+
+#set -e
+
+CHANNEL_NAME=$1
+: ${CHANNEL_NAME:="mychannel"}
+echo $CHANNEL_NAME
+
+## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
+function generateChannelArtifacts() {
+
+	CONFIGTXGEN=`pwd`/bin/configtxgen
+
+	if [ -f "$CONFIGTXGEN" ]; then
+            echo "Using configtxgen -> $CONFIGTXGEN"
+	else
+	    exit 1
+	fi
+
+	rm -rf channel-artifacts
+	mkdir channel-artifacts
+
+	echo "##########################################################"
+	echo "#########  Generating Orderer Genesis block ##############"
+	echo "##########################################################"
+	$CONFIGTXGEN -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/orderer.genesis.block
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile ThreeOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org2MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+	echo
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org3MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile ThreeOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org3MSP
+	echo
+}
+
+generateChannelArtifacts
+
